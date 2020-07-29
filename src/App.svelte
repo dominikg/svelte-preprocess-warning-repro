e5495d7bc46ed0f781fead6171afcae1301e44cd
dominikg <dominik.goepel@gmx.de>
2020-07-29 23:38:12 +0200

    "initial commit"

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script type="text/javascript">
+  import Nested from './Nested.svelte';
+  export let label = '';
+
+  $: console.log(label);
+</script>
+
+<style type="text/stylus">
+  /* prettier-ignore */
+  .note
+    color #ff3e00
+  h1
+    color blue
+</style>
+
+<!-- prettier-ignore -->
+<template type="text/pug">
+  h1.note This is the svelte-preproces example - built with svite
+  hr
+  h1 Hello!
+  br
+  input(bind:value="{label}")
+  br
+  Nested("{label}")
+</template>
