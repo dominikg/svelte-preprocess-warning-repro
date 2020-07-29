bd8a60cebf29423e4a52176a1dc9e698e259dbe6
dominikg <dominik.goepel@gmx.de>
2020-07-29 23:40:58 +0200

    add preprocessor config that creates warnings

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,27 @@
 const preprocess = require('svelte-preprocess');
 module.exports = {
+  preprocess: [
+    preprocess({
+      // disable all but typescript
+      babel: false,
+      scss: false,
+      sass: false,
+      less: false,
+      stylus: false,
+      postcss: false,
+      coffeescript: false,
+      pug: false,
+      globalStyle: false,
+      replace: false,
+    }),
+    preprocess({typescript:false})
+  ],
+};
+// this one works without warnings
+/*
+module.exports = {
   preprocess: preprocess({
     typescript: false, // for typescript, use the typescript examples!
   }),
 };
+*/
